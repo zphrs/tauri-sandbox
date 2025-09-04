726066848acf6c23792a62501923b4cd8044f49b
zphrs <z@zephiris.dev>
2025-09-04 14:48:35 -0700

    formatting

--- tauri-app/subdomain/src/main.ts
+++ tauri-app/subdomain/src/main.ts
@@ -3,42 +3,42 @@ import {
   overrideCookie,
   overrideIndexedDB,
   overrideLocalStorage,
-} from "frame-glue"
+} from "frame-glue";
 
 const setupPromises = [
   domReplacement(),
   overrideIndexedDB(),
   overrideCookie(),
   overrideLocalStorage(new URL(origin).pathname.slice(1)),
-]
+];
 
 function refreshPort() {
-  window.parent.postMessage("iframe refresh port", "*")
+  window.parent.postMessage("iframe refresh port", "*");
 }
 
 async function initSwProxy(port: MessagePort) {
   if (!("serviceWorker" in navigator))
-    throw new Error("Service worker is unsupported.")
-  console.log("initing sw")
-  const existingReg = await navigator.serviceWorker.getRegistration()
+    throw new Error("Service worker is unsupported.");
+  console.log("initing sw");
+  const existingReg = await navigator.serviceWorker.getRegistration();
   const onInstalled = (controller: ServiceWorker) => {
-    console.log("sw installed")
-    controller.postMessage("init-sw-proxy", [port])
-    console.log("posted port")
-    controller.addEventListener("error", e => {
-      console.log("SW ERR", e)
-    })
+    console.log("sw installed");
+    controller.postMessage("init-sw-proxy", [port]);
+    console.log("posted port");
+    controller.addEventListener("error", (e) => {
+      console.log("SW ERR", e);
+    });
     navigator.serviceWorker.addEventListener(
       "message",
-      e => {
-        console.log("MSG", e)
+      (e) => {
+        console.log("MSG", e);
         if (e.data == "refresh port") {
-          refreshPort()
+          refreshPort();
         }
       },
       { once: true }
-    )
-  }
+    );
+  };
   const reg =
     existingReg ??
     (await navigator.serviceWorker.register(
@@ -47,34 +47,40 @@ async function initSwProxy(port: MessagePort) {
         type: import.meta.env.MODE === "production" ? "classic" : "module",
         scope: "/",
       }
-    ))
+    ));
   if (navigator.serviceWorker.controller && reg.active) {
-    onInstalled(navigator.serviceWorker.controller)
-    return
+    onInstalled(navigator.serviceWorker.controller);
+    return;
   }
-  console.log(await navigator.serviceWorker.getRegistrations())
-  navigator.serviceWorker.addEventListener("controllerchange", async e => {
-    console.log(e)
+  console.log(await navigator.serviceWorker.getRegistrations());
+  navigator.serviceWorker.addEventListener("controllerchange", async (e) => {
+    console.log(e);
     if (navigator.serviceWorker.controller)
-      onInstalled(navigator.serviceWorker.controller)
+      onInstalled(navigator.serviceWorker.controller);
     else
-      console.warn("controller change happened and controller is still null", e)
-  })
+      console.warn(
+        "controller change happened and controller is still null",
+        e
+      );
+  });
   // unregister to clean up this sw when the page closes.
   // deregistration only applies after all active clients close
 
   // reg.unregister()
 }
 window.addEventListener("message", async (event: MessageEvent<any>) => {
-  console.log(event)
+  console.log(event);
   if (event.data == "init-proxied-sw port") {
-    console.log("init-proxied-sw", event.data, event.ports.length)
-    const portForSw = event.ports[0]
+    console.log("init-proxied-sw", event.data, event.ports.length);
+    const portForSw = event.ports[0];
     // sleep to wait for deregistration of the old service worker,
     // if an old sw exists
     initSwProxy(portForSw).then(() => {
-      window.parent.postMessage("init-proxied-sw port inited", "*")
-    })
+      window.parent.postMessage("init-proxied-sw port inited", "*");
+    });
   }
-})
-window.parent.postMessage("iframe inited", "*")
+});
+window.parent.postMessage("iframe inited", "*");
+Promise.all(setupPromises).then(() => {
+  // TODO
+});
